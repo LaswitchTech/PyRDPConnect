9b9735535905044fd459072e9314634db81d02a7
Louis Ouellet <louis_ouellet@hotmail.com>
2025-12-03 12:27:08 -0500

    General: Tiny update to improve the readability of the help message.

--- src/app/cli.py
+++ src/app/cli.py
@@ -77,6 +77,7 @@ class CommandLine(QApplication):
     # ------------------------------------------------------------------
 
     def help(self) -> None:
+        print()
         print(f"Usage: {self.name} [command] [options]")
         print()
         print("Available commands:")
@@ -111,6 +112,8 @@ class CommandLine(QApplication):
 
         for usage, desc in entries:
             print(f"  {usage:<{max_cmd_len}}  {desc}")
+
+        print()
 
     def add(self, command: str, description: str = "", callable: Optional[callable] = None, args: int = 0, arg_names: Optional[list[str]] = None) -> None:
         if not command.startswith("--"):
