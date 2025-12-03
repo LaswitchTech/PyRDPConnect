e0ad0a6c263222e5f848646a177ab56b062cd2ca
Louis Ouellet <louis_ouellet@hotmail.com>
2025-12-03 12:15:14 -0500

    General: Completed the implementation of the command-line in the core framework.

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def start_cli():
     cli = CommandLine(name,sys.argv)
 
     # Add test command
-    cli.add("test", "A test command", lambda: print("Hello World!"))
+    cli.add("test", "A test command", lambda x: print(f"Hello {x}!"), args=1, arg_names=["<name>"])
 
     # All other code gets app via QApplication.instance()
     sys.exit(cli.exec())
